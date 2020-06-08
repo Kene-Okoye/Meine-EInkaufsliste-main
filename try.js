4e34f944eba66b3065c605ed6f3bf04add63d050
Kene_Okoye <mr.kene.okoye@gmail.com>
2020-06-08 14:57:46 +0200

    Major change a Line 22: added code to remove previously created lists in main HTML on click of the 'createNewList' button

--- try.js
+++ try.js
@@ -10,24 +10,33 @@ let allList;
 // 2.) Add event listner for creating new list.
 createNewListButton.addEventListener("click", createNewList);
 
-// 3.) FUnction to create new list.
+// 3.) FUnctions to create new list.
 function createNewList() {
-    document.body.removeChild(createNewListButton);
+    hidecreateNewList();
+    showCreateNewList();
+}
+
+function hidecreateNewList() {
     document.body.removeChild(newListTeaser);
+    document.body.removeChild(createNewListButton);
+    document.querySelectorAll('.list-div-container').forEach(e => {
+        e.remove();
+    });
+}
+
+function showCreateNewList() {
     const newListContainer = document.createElement("div");
     newListContainer.classList.add("new-list-container");
     const listInfoMessage = document.createElement("p");
     listInfoMessage.classList.add("list-info-message");
     listInfoMessage.innerText = "Enter list name";
+
     const newListInput = document.createElement("input");
     newListInput.classList.add("new-list-input");
+
     const newListButton = document.createElement("button");
     newListButton.classList.add("new-list-button");
     newListButton.innerText = "Create list";
-    newListContainer.appendChild(listInfoMessage);
-    newListContainer.appendChild(newListInput);
-    newListContainer.appendChild(newListButton);
-    document.body.appendChild(newListContainer);
     newListButton.addEventListener("click", (displayList) => {
         const singleList = {
             name: newListInput.value,
@@ -36,7 +45,7 @@ function createNewList() {
         if (newListInput.value !== "") {
             myLists.push(singleList);
             allList = singleList;
-            //console.log(allList);
+            console.log(allList.name);
             const listContainerDiv = document.createElement("div");
             listContainerDiv.classList.add("list-div-container");
             const newListName = document.createElement("h1");
@@ -54,57 +63,8 @@ function createNewList() {
             window.alert("Please insert an input.");
         }
     });
+    newListContainer.appendChild(listInfoMessage);
+    newListContainer.appendChild(newListInput);
+    newListContainer.appendChild(newListButton);
+    document.body.appendChild(newListContainer);
 }
-
-
-
-
-
-/* const docContainer = document.querySelector(".DOC-container");
-
-
-// <--- GLOBAL VARIABLES --->
-
-
-// <--- GLOBAL FUNCTIONS CALLS --->
-createNewList();
-
-// < --- GLOBAL FUNCTION DEFINITIONS --->
-function createNewList() {
-    // 1.) Create elements needed for NEW LIST
-    const newListInput = document.createElement("input");
-    const newListButton = document.createElement("button");
-    newListButton.innerText = "Create list";
-    // 2.) Attach them to the DOC-container
-    docContainer.appendChild(newListInput);
-    docContainer.appendChild(newListButton);
-
-    // 3.) Display list NAME on DOC-container on click
-    // Add an event lister
-    newListButton.addEventListener("click", (displayList) => {
-        // 2b.) Ensure a user inserts an input before creating the NEW LIST then
-        // remove all other elements and show only NEW LIST attached
-        // to LIST-DIV-CONTAINER
-        if (newListInput.value !== "") {
-            docContainer.removeChild(newListInput);
-            docContainer.removeChild(newListButton);
-            window.alert(`${newListInput.value} has been created.`);
-            createNewList();
-        }
-        else {
-            window.alert("Please insert an input.");
-        }
-        // 2a.) Display the created NEW LIST
-        const listContainerDiv = document.createElement("div");
-        const newList = document.createElement("div");
-        newList.innerText = newListInput.value;
-        listContainerDiv.appendChild(newList);
-        docContainer.appendChild(listContainerDiv);
-
-        // 3.) Edit list after its created by clicking on LIST name
-        // Add an event lister
-        newList.addEventListener("click", (editList) => {
-            newList.contentEditable = true;
-        });
-    });
-} */
